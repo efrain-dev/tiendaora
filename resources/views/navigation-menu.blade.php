124ddd5a3a1790962d8a5c6934641258da50babf
Efrain de Leon <efraindeleon12@outlook.com>
2021-06-05 02:04:59 -0600

    cambios

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -9,7 +9,8 @@
                 <!-- Logo -->
                 <div class="flex-shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-jet-application-mark class="block h-9 w-auto"/>
+
+                        <x-jet-application-mark class="block h-12 w-auto hover:translate-x-0.5 transition-all" />
                     </a>
                 </div>
 
